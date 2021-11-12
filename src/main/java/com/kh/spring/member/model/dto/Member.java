f506edfe1a28695f4939447596cf87b2751c2747
cxyxj98 <cxyxj98@gmail.com>
2021-11-12 10:35:16 +0900

    [윤지]테이블 수정 회원가입

--- src/main/java/com/kh/spring/member/model/dto/Member.java
+++ src/main/java/com/kh/spring/member/model/dto/Member.java
@@ -6,71 +6,73 @@ import java.sql.Date;
 
 public class Member {
    
+   private String userIdx;
    private String email;
    private String password;
    private String nickname;
    private String git;
    private Date regDate;
-   private String profileImg;
-   private int profileColor;
+   private String profileColor;
    private int isLeave;
    
-   public String getEmail() {
-      return email;
-   }
-   public void setEmail(String email) {
-      this.email = email;
-   }
-   public String getPassword() {
-      return password;
-   }
-   public void setPassword(String password) {
-      this.password = password;
-   }
-   public String getNickname() {
-      return nickname;
-   }
-   public void setNickname(String nickname) {
-      this.nickname = nickname;
-   }
-   public String getGit() {
-      return git;
-   }
-   public void setGit(String git) {
-      this.git = git;
-   }
    
-   public Date getRegDate() {
-      return regDate;
-   }
-   public void setRegDate(Date regDate) {
-      this.regDate = regDate;
-   }
-   public String getProfileImg() {
-      return profileImg;
-   }
-   public void setProfileImg(String profileImg) {
-      this.profileImg = profileImg;
-   }
-   public int getProfileColor() {
-      return profileColor;
-   }
-   public void setProfileColor(int profileColor) {
-      this.profileColor = profileColor;
-   }
-   public int getIsLeave() {
-      return isLeave;
-   }
-   public void setIsLeave(int isLeave) {
-      this.isLeave = isLeave;
-   }
-@Override
-public String toString() {
-	return "Member [email=" + email + ", password=" + password + ", nickname=" + nickname + ", git=" + git
-			+ ", regDate=" + regDate + ", profileImg=" + profileImg + ", profileColor=" + profileColor + ", isLeave="
-			+ isLeave + "]";
-}
-   
+		public String getUserIdx() {
+			return userIdx;
+		}
+		public void setUserIdx(String userIdx) {
+			this.userIdx = userIdx;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getNickname() {
+			return nickname;
+		}
+		public void setNickname(String nickname) {
+			this.nickname = nickname;
+		}
+		public String getGit() {
+			return git;
+		}
+		public void setGit(String git) {
+			this.git = git;
+		}
+		public Date getRegDate() {
+			return regDate;
+		}
+		public void setRegDate(Date regDate) {
+			this.regDate = regDate;
+		}
+		public String getProfileColor() {
+			return profileColor;
+		}
+		public void setProfileColor(String profileColor) {
+			this.profileColor = profileColor;
+		}
+		public int getIsLeave() {
+			return isLeave;
+		}
+		public void setIsLeave(int isLeave) {
+			this.isLeave = isLeave;
+		}
+		@Override
+		public String toString() {
+			return "Member [userIdx=" + userIdx + ", email=" + email + ", password=" + password + ", nickname=" + nickname
+					+ ", git=" + git + ", regDate=" + regDate + ", profileColor=" + profileColor + ", isLeave=" + isLeave + "]";
+		}
+		   
+		   
+		   
+  
 
 
    
