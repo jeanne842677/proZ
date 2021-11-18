c0af31d0b20f2b686048ebe9496d7f586caedcc1
cxyxj98 <cxyxj98@gmail.com>
2021-11-18 17:45:03 +0900

    [윤지] 기본 어노테이션 추가

--- src/main/java/com/kh/spring/memo/controller/MemoController.java
+++ src/main/java/com/kh/spring/memo/controller/MemoController.java
@@ -1,10 +1,13 @@
 package com.kh.spring.memo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import com.kh.spring.board.model.service.BoardService;
+import com.kh.spring.memo.model.service.MemoService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +16,10 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("memo")
 public class MemoController {
 
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	private final MemoService memoService;
+	
 	@GetMapping("memo")
 	public void boardForm() {};
 }
--- src/main/java/com/kh/spring/memo/model/service/MemoServiceImpl.java
+++ src/main/java/com/kh/spring/memo/model/service/MemoServiceImpl.java
@@ -1,5 +1,9 @@
 package com.kh.spring.memo.model.service;
 
-public class MemoServiceImpl {
+import com.kh.spring.board.model.repository.BoardRepository;
+import com.kh.spring.memo.model.repository.MemoRepository;
 
+public class MemoServiceImpl implements MemoService{
+
+	//private final MemoRepository memoRepository;
 }
