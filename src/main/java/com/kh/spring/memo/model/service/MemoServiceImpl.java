4b3f561708e55539e27d05c276b6437da3fc8fec
cxyxj98 <cxyxj98@gmail.com>
2021-11-18 17:52:49 +0900

    [윤지] service 어노테이션추가

--- src/main/java/com/kh/spring/memo/model/service/MemoServiceImpl.java
+++ src/main/java/com/kh/spring/memo/model/service/MemoServiceImpl.java
@@ -1,8 +1,10 @@
 package com.kh.spring.memo.model.service;
 
-import com.kh.spring.board.model.repository.BoardRepository;
+import org.springframework.stereotype.Service;
+
 import com.kh.spring.memo.model.repository.MemoRepository;
 
+@Service
 public class MemoServiceImpl implements MemoService{
 
 	//private final MemoRepository memoRepository;
