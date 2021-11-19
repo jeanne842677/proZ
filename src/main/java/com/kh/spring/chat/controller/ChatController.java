fd6b7ebf5cfb51886273112865c53fd52e025ae6
sazzeo <imjee02@gmail.com>
2021-11-19 11:37:32 +0900

    [채팅 ] 커밋

--- src/main/java/com/kh/spring/chat/controller/ChatController.java
+++ src/main/java/com/kh/spring/chat/controller/ChatController.java
@@ -7,6 +7,7 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -30,6 +31,7 @@ public class ChatController {
         
     }
     
+
     @RequestMapping("chat/chatting")
     public void chatting() {};
 
