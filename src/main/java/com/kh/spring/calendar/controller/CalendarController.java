1e7e7ca773421c8a497909742b8be10f4ddd9fe1
sazzeo <imjee02@gmail.com>
2021-11-26 19:56:33 +0900

    [지영] 캘린더 시작

--- src/main/java/com/kh/spring/calendar/controller/CalendarController.java
+++ src/main/java/com/kh/spring/calendar/controller/CalendarController.java
@@ -1,5 +1,23 @@
 package com.kh.spring.calendar.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("calendar")
 public class CalendarController {
+	
+	
+	@GetMapping("{projectIdx}")
+	public String calendar(@PathVariable String projectIdx 
+			, @RequestParam String wsIdx) {
+		
+		
+		return "/calendar/calendar";
+	}
+	
 
 }
