dea73e98e71e9e095ccb87e1ae0a123745273627
sazzeo <imjee02@gmail.com>
2021-11-30 10:49:34 +0900

    [지영] 파이어베이스 컨피그 수정

--- src/main/java/com/kh/spring/fcm/FirebaseConfig.java
+++ src/main/java/com/kh/spring/fcm/FirebaseConfig.java
@@ -1,27 +1,42 @@
 package com.kh.spring.fcm;
 
+import java.io.File;
 import java.io.FileInputStream;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
+import io.grpc.internal.SharedResourceHolder.Resource;
+
 @Configuration
 public class FirebaseConfig {
 	private FirebaseOptions option;
-	private final static String PATH = "C:\\CODE\\z_final_Project\\src\\main\\webapp\\resources\\fcm\\proz-78541-firebase-json.json";
+	private String PATH = "resources\\fcm\\proz-78541-firebase-json.json";
+	
 	
 	@PostConstruct
 	private void init() throws Exception {
-		FileInputStream refreshToken = new FileInputStream(PATH);
+		
+		
+		String rootPath =  this.getClass().getResource("/").getPath()+ "../../" +PATH;
+				
+		
+		FileInputStream refreshToken = new FileInputStream(rootPath);
 		option = new FirebaseOptions.Builder()
 				.setCredentials(GoogleCredentials.fromStream(refreshToken))
 				.setDatabaseUrl("https://proz-78641.firebaseio.com")
 				.build();
 		FirebaseApp.initializeApp(option);
 	}
+	
+	
+	
 }
